8c7cd939d6bf859e35454ef65b1aaee3c1f5c7f7
GitHub Action <action@github.com>
2025-05-31 13:15:14 +0900

    update formula to v1.0.5

--- Formula/aictl.rb
+++ Formula/aictl.rb
@@ -1,15 +1,25 @@
 class Aictl < Formula
     desc "Handy CLI tool to ask anything to generative AI in command line."
     homepage "https://github.com/go-zen-chu/aictl"
-    version "1.0.4"
+    version "1.0.5"
     
     on_macos do
         if Hardware::CPU.arm?
-            url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.4/aictl_Darwin_arm64.tar.gz"
-            sha256 "d7703233c5a6e7c4dfad95ebc66a0a569adacdfc9a60ada335710919e02d274e"
+            url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.5/aictl_Darwin_arm64.tar.gz"
+            sha256 ""
         else
-            url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.4/aictl_Darwin_x86_64.tar.gz"
-            sha256 "8d308147f1ce0be96c7ccd81a27b619da913384dab819e6486c2cfe723691890"
+            url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.5/aictl_Darwin_x86_64.tar.gz"
+            sha256 ""
+        end
+    end
+
+    on_linux do
+        if Hardware::CPU.arm?
+            url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.5/aictl_Linux_arm64.tar.gz"
+            sha256 ""
+        else
+            url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.5/aictl_Linux_x86_64.tar.gz"
+            sha256 ""
         end
     end
 
